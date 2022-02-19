5786dca90d68f36212788814943f7eb6f7801e78
KANEA MACDONALD <kanea.macdonald@gmail.com>
2022-02-19 15:44:26 -0600

    Moved Images to Public Folder

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -1,8 +1,7 @@
-import avatar from "./avatar.png";
 export const Bio = () => {
   return (
     <div className="flex">
-      <img src={avatar} alt="it's a me" className=" w-40 h-40 rounded-full" />
+      <img src={`${process.env.PUBLIC_URL}/avatar.png`} alt="it's a me" className=" w-40 h-40 rounded-full" />
       <div>
         <h3 className="text-teal-900 text-3xl">Kanea MacDonald</h3>
         <h4 className="text-teal-800 text-2xl">Good at Coding (One Day)</h4>
